e7f9124186d30e6f426cbfccdadae7dbc72312d6
Jerry Hobby <Jerry@JerryHobby.com>
2015-03-18 23:57:18 -0500

    PyService initial commit

---
+++ PyService/net_client.py
@@ -0,0 +1,20 @@
+# echo_client.py 
+
+import socket 
+
+host = socket.gethostname() 
+port = 12345 # The same port as used by the server 
+
+text = [b'Hello World', b'Work goes here.', b'Goodbye World', b'quit' ]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect((host, port)) 
+
+for msg in text:
+	s.sendall(msg) 
+
+	data = s.recv(1024) 
+	print('Received', repr(data)) 
+
+s.close() 
+
